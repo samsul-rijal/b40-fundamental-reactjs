b95c821ec3f383c82adf3f9a4d6703f774513a8e
Your Name <samsulr99@gmail.com>
2022-07-19 14:52:12 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import EmbedExpression from "./embedExpression";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <EmbedExpression />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
