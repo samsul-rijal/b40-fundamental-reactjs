2c6c6549926f654ba2a812ecca8a9861e0281cbe
Your Name <samsulr99@gmail.com>
2022-07-19 15:01:40 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import State from "./state";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <State />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
