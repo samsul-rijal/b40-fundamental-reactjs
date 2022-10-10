335f4a933004c99380beb31a23f29da87b4cde72
Your Name <samsulr99@gmail.com>
2022-10-10 13:13:46 +0700

    update

--- src/conditionRendering.js
+++ src/conditionRendering.js
@@ -1,14 +1,39 @@
 import React from "react";
+import { useState } from "react";
 
 //Create Component PrivatePage
+function PrivatePage(props) {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <button onClick={props.logout}>Logout</button>
+    </div>
+  );
+}
 
 //Create Component GuestPage
+function GuestPage(props) {
+  return (
+    <div>
+      <h1>Please Sign</h1>
+      <button onClick={props.login}>Login</button>
+    </div>
+  );
+}
 
 function ConditionRendering() {
   //init State
+  const [isLogin, setIsLogin] = useState(false)
+
   return (
     // Code Inside div
-    <div>{/* conditional logic rendering */}</div>
+    <div>
+      {isLogin ? 
+        <PrivatePage logout={() => setIsLogin(false)} /> 
+        : 
+        <GuestPage login={() => setIsLogin(true)} />
+      }
+    </div>
   );
 }
 
