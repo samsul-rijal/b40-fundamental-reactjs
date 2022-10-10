68949363f618094a307aa388c6294860e786a022
Your Name <samsulr99@gmail.com>
2022-10-10 11:00:45 +0700

    update

--- src/event.js
+++ src/event.js
@@ -3,9 +3,16 @@ import React from "react";
 function Event() {
   //Create function here
 
+  function Greeting(){
+    return alert("good morning everyone have a nice day")
+  }
+
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <button onClick={Greeting}>Klik</button>
+      <button onClick={() => alert("Saya diklik")}>Klik ME</button>
+    </div>
   );
 }
 
